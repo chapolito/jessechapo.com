7b8385827e720f0e812a6ab4936aea15adc2ce76
Jesse Chapo <jessechapo@gmail.com>
2013-02-24 14:33:31 -0800

    polished blog, added responsiveness and proof read.

--- jessechapo.com/_site/js/main.js
+++ jessechapo.com/_site/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {  
-    $('input[type=email]').addClass("idleField")
+    $('input[type="email"]').addClass("idleField")
     .focus(function() {  
         $(this).removeClass("idleField").removeClass("completedField").addClass("focusField");  
         if (this.value == this.defaultValue){  
@@ -19,3 +19,26 @@ $(document).ready(function() {
         }
     });  
 });  
+
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
