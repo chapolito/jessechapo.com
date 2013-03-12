0e9bece038809de9a41e5a633dce709fb5cb2416
Jesse Chapo <jessechapo@gmail.com>
2013-03-12 09:04:51 -0700

    added the new footnotes to the site.

--- jessechapo.com/js/main.js
+++ jessechapo.com/js/main.js
@@ -1,7 +1,61 @@
-$(document).ready(function() {  
-    $('input[type="email"]').addClass("idleField")
+$(document).ready(function() { 
+
+    var $fn = $("#currentFootnote div");
+    $fn.parent().hide();
+    var fnIsDismissed = true;
+
+    $('sup > a').click(function() {
+        $fnRef = $(this).attr("name");
+        $fnText = $($fnRef).html().trim().split('<a href="#')[0];
+        if (fnIsDismissed) {
+            $fn.html($fnText).parent().animate({
+                height: "toggle",
+                opacity: "toggle"
+            }, 'slow');
+            fnIsDismissed = false; 
+        }  else {
+            $fn.parent().animate({
+                height: "toggle",
+                opacity: "toggle"
+            }, '1000');
+            if ($fnText != $fn.html()) { //if footnote clicked is not the one already being displayed
+                setTimeout(function() {
+                    $fn.html($fnText).parent().animate({
+                        height: "toggle",
+                        opacity: "toggle"
+                    }, 'slow');
+                    fnIsDismissed = false; 
+                }, 800);
+            } else fnIsDismissed = true; //if it is itself just leave dismissed
+        }
+
+        var $window = $(window);
+        var $startingScrollHeight = $window.scrollTop();
+        var deltaScroll = 0;
+        $(window).scroll(function() {
+            deltaScroll = ($window.scrollTop() - $startingScrollHeight);
+            if ((deltaScroll > 500 || deltaScroll < -500) && !fnIsDismissed) {
+                $fn.parent().animate({
+                    height: "toggle",
+                    opacity: "toggle"
+                }, '1000');
+                fnIsDismissed = true; 
+            }
+        }); 
+    });
+
+    $fn.parent().click(function(evt) {
+        if ($(evt.target).attr('href')) return true;
+        $fn.parent().animate({
+            height: "toggle",
+            opacity: "toggle"
+        }, 'slow');
+        fnIsDismissed = true;
+    })
+    
+    $('input[type=email]').addClass('idleField')
     .focus(function() {  
-        $(this).removeClass("idleField").removeClass("completedField").addClass("focusField");  
+        $(this).removeClass('idleField').removeClass('completedField').addClass('focusField');  
         if (this.value == this.defaultValue){  
             this.value = '';  
         }  
@@ -10,35 +64,12 @@ $(document).ready(function() {
         }  
     })
     .blur(function() {  
-        $(this).removeClass("focusField");
+        $(this).removeClass('focusField');
         if ($.trim(this.value) == ''){  
-            $(this).addClass("idleField"); 
+            $(this).addClass('idleField'); 
             this.value = (this.defaultValue ? this.defaultValue : '');  
         }  else {
-            $(this).addClass("completedField");
+            $(this).addClass('completedField');
         }
     });  
 });  
-
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
